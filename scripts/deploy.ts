b44b6cbb226c7afb769cfd268d40bd7b871223b2
William Muli <willi.wambu@gmail.com>
2023-04-12 16:46:57 +0300

    Add deployment details for sepolia

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,5 @@
-import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
-
-  const lockedAmount = ethers.utils.parseEther("0.001");
-
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
-}
-
-async function deployElectoralCommissionContract() {
   const ElectoralCommission = await ethers.getContractFactory("ElectoralCommission");
   const electoralCommission = await ElectoralCommission.deploy();
 
@@ -28,11 +11,6 @@ async function deployElectoralCommissionContract() {
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error('Lock deployment error: ', error);
-  process.exitCode = 1;
-});
-
-deployElectoralCommissionContract().catch((error) => {
   console.error('Electoral Commission deployment Error:', error);
   process.exitCode = 1;
 });
--- scripts/electoral-commission.ts
+++ scripts/electoral-commission.ts
@@ -1,17 +1,20 @@
 const { ethers } = require("hardhat")
 
 async function main() {
-  const contractAddress = '0x9A9f2CCfdE556A7E9Ff0848998Aa4a0CFD8863AE';
+  const contractAddress = '0xB7f8BC63BbcaD18155201308C8f3540b07f84F5e';
   const ElectoralCommission = await ethers.getContractFactory('ElectoralCommission')
   const electoralCommission = await ElectoralCommission.attach(contractAddress)
   const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
   const endTime = Date.now() + ONE_YEAR_IN_SECS;
-  const tx = await electoralCommission.createElection("Chairperson Election 2023", "Chairperson", Date.now(), endTime)
-  const receipt = await tx.wait()
+  // const tx = await electoralCommission.createElection("Chairperson Election 2023", "Chairperson", Date.now(), endTime)
+  // const receipt = await tx.wait()
+
+  const elections = await electoralCommission.getElections()
+  console.log(elections)
   
-  electoralCommission.on('ElectionCreatedEvent', (electionId) => {
-    console.log({ electionId: electionId })
-  })
+  // electoralCommission.on('ElectionCreatedEvent', (electionId) => {
+  //   console.log({ electionId: electionId })
+  // })
 }
 
 main().catch((error) => {
